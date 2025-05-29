e85b945a6cd3961b14359e0f4039057079f2d7b8
Bugleap-dev <bugleap.dev@gmail.com>
2025-05-29 08:26:07 +0100

    UPDATE

---
+++ day-11(24-05-2025)/code-11.py
@@ -0,0 +1,113 @@
+def main():
+    print_column(3)
+
+def print_column(height):
+    print("#\n" * height,end="")
+
+main()
+
+"""
+Output:
+
+###
+###
+###
+
+"""
+
+############
+
+def main():
+    print_row(4)
+
+def print_row(width):
+    print("#"*width)
+
+
+main()
+"""
+Output:
+
+####
+####
+####
+####
+
+"""
+
+#############
+
+def main():
+    print_square(4)
+
+def print_square(size):
+
+    #for each row in square
+    for i in range(size):
+
+        #for each brick in row
+        for j in range(size):
+            print("#", end="")
+            
+        print()    
+
+
+main()
+
+"""
+Output:
+
+####
+####
+####
+####
+
+"""
+
+################
+
+def main():
+    print_square(4)
+
+
+def print_square(size):
+    for i in range(size):
+        print("#"*size)
+
+
+main()
+
+"""
+Output:
+
+####
+####
+####
+####
+
+"""
+
+################
+
+def main():
+    print_square(4)
+
+
+def print_column(size):
+    for i in range(size):
+        print_row(size)
+
+
+def print_row(width):
+    print("#"*width)
+main()
+
+"""
+Output:
+
+####
+####
+####
+####
+
+"""
